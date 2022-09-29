eb413019c849d27556c493ea094c35c7c316ba80
Kaustav2004 <112540025+Kaustav2004@users.noreply.github.com>
2022-09-30 00:28:31 +0530

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a , b;
+	cout<<"Enter the value of value of a "<<endl;
+	cin>>a;
+	cout<<"Enter the value of value of b"<<endl;
+	cin>>b;
+	cout<<"Value of a = "<<a<<endl;
+	cout<<"Value of b = "<<b<<endl;
+
+	//addition
+	cout<<"sum = "<<a+b<<endl;
+	
+	return 0;
+}
