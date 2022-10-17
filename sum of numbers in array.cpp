c827f3ace3ed7b6d946f0474069da9481217d89e
Kaustav2004 <112540025+Kaustav2004@users.noreply.github.com>
2022-10-17 21:46:47 +0530

    Update sum of numbers in array.cpp

--- sum of numbers in array.cpp
+++ sum of numbers in array.cpp
@@ -3,12 +3,13 @@ using namespace std;
 //sum of numbers in array
 int main(){
 	int size,k;
-	cout<<"you can put max. 10 numbers"<<endl;
-	int box[10];
-	for(int i=0;i<=10;i++){
+	cout<<"put how many number you want to put(max. 1L)"<<endl;
+	cin>>size;
+	int box[100000];
+	for(int i=1;i<=size;i++){
 		cin>>box[i];
 	}
-	for(int i=0;i<=10;i++){
+	for(int i=0;i<=size;i++){
 		k=k+box[i];
 	}
 	cout<<"Sum is = "<<k<<endl;
